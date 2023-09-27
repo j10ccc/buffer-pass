967f67d97380b3e78f5b15f0ed42a7906674b152
j10c <blyb1739@gmail.com>
2023-09-27 21:13:09 +0800

    feat(userscript/bundle): add meta & license banner

--- packages/userscript/rollup.config.mjs
+++ packages/userscript/rollup.config.mjs
@@ -1,18 +1,32 @@
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import nodePolyfills from "rollup-plugin-polyfill-node";
+import { userScriptPlugin } from "rollup-plugin-userscript-metaheader";
+
+import fs from "fs";
+import pkg from "./package.json" assert { type: "json" };
 
 export default {
   input: "src/main.ts",
   output: {
-    file: "dist/main.js",
-    format: "iife"
+    file: "dist/bundle.js",
+    format: "iife",
+    banner: () => ("\n/*\n" + fs.readFileSync("../../LICENSE", "utf8") + "\n*/\n"),
   },
   plugins: [
     nodePolyfills(),
     typescript(),
     resolve({
       browser: true,
+    }),
+    userScriptPlugin({
+      headers: {
+        name: pkg.name,
+        description: pkg.description,
+        version: pkg.version,
+        author: pkg.author,
+        license: pkg.license
+      }
     })
   ]
 };
